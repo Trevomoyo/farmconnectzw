a453ad9e065147b7784b00d3f79018fed9ed17a6
Trevor Moyo <164605423+Trevomoyo@users.noreply.github.com>
2026-05-03 20:15:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ if (process.env.VAPID_PUBLIC_KEY && process.env.VAPID_PRIVATE_KEY) {
                     "ws:", 
                     "wss:" 
                    ],
-       frameSrc:   ["'self'", "https://farmconnectzw.firebaseapp.com", "https://farmconnectzw.web.app",
+       frameSrc:   ["'self'", "https://farmconnectzw.firebaseapp.com","https://farmconnectzw.co.zw","https://farmconnectzw.web.app",
                     "https://accounts.google.com"],
        workerSrc:  ["'self'", "blob:"],
        mediaSrc:   ["'self'", supabaseUrl ? `${supabaseUrl}/*` : ""]
@@ -107,6 +107,7 @@ if (process.env.VAPID_PUBLIC_KEY && process.env.VAPID_PRIVATE_KEY) {
 const ALLOWED_ORIGINS = [
   'https://farmconnectzw.web.app',
   'https://farmconnectzw.firebaseapp.com',
+  'https://farmconnectzw.co.zw',
   process.env.RENDER_EXTERNAL_URL || 'https://farmconnectzw.onrender.com'
 ].filter(Boolean);
 
